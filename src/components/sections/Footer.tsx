f1b912bd4cd2b5cddcfdc636a3e94ff7cf1e8b52
cjorgeluis122333 <cjorgeluis122333@gmail.com>
2026-03-04 20:43:28 -0500

    Change the boody

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -4,11 +4,7 @@ export const Footer: React.FC = () => {
     return (
         <footer className="border-t border-white/5 py-20 text-center bg-black/30">
             <p className="text-accent-muted text-lg font-medium">
-                © {new Date().getFullYear()} Jorge Luis Castillo Vidal. Implementación Full-Stack & Mobile.
-            </p>
-            <p className="text-accent-muted/50 text-sm mt-4 tracking-widest uppercase">
-                Diseño Premium • React 19 • Tailwind CSS v3
-            </p>
+                © {new Date().getFullYear()} Jorge Luis Castillo Vidal.</p>
         </footer>
     );
 };
